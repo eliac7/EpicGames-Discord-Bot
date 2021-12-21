63aedc3b8b77e529f9a4971ab9ae11722886c852
Ilias <elias.thal7@gmail.com>
2021-12-21 18:12:29 +0200

    Fix cron (?)

--- index.js
+++ index.js
@@ -18,52 +18,44 @@ client.on("ready", () => {
 client.on("ready", (client) => {
   //Send a message on a channel , every Thursday on 6PM.
 
-  //First check if is sent
-  let isSend = false;
-  let cron = "* 0 18 * * 4";
+  let cron = "0 18 * * 4";
   //Christmas cron
-  cron = "* 18 * * *";
+  cron = "0 18 * * *";
   schedule.scheduleJob(cron, async () => {
-    //If it is not sent, send it
-    if (!isSend) {
-      isSend = true;
-      const i = await getGames("latest");
+    const i = await getGames("latest");
 
-      //Send a message on channel before "spamming" with games
-      let title = "**LATEST GAMES**";
-      //Christmas title
-      title = "**LATEST GAMES (Christmas Edition 🎅)**";
-      client.channels.cache.get("709045710204436540").send(title);
-      i.forEach((data) => {
-        const embed = new MessageEmbed()
-          .setColor("#f2f2f2")
-          .setTitle(data.title)
-          .setURL(data.productURL)
-          .setDescription(data.description)
-          .addFields(
-            {
-              name: "Started on",
-              value: data.startDate,
-              inline: true,
-            },
-            {
-              name: "Ends on",
-              value: `${data.endDate} ${
-                data.endDateDiffernce > 1
-                  ? `( In ${data.endDateDiffernce} days )`
-                  : `( In ${data.endDateDiffernce} day )`
-              }`,
-              inline: true,
-            }
-          )
-          .setImage(data.imageURL)
-          .setTimestamp()
-          .setFooter("Made by eliac7#5541");
-        client.channels.cache
-          .get("709045710204436540")
-          .send({ embeds: [embed] });
-      });
-    }
+    //Send a message on channel before "spamming" with games
+    let title = "**LATEST GAMES**";
+    //Christmas title
+    title = "**LATEST GAMES (Christmas Edition 🎅)**";
+    client.channels.cache.get("709045710204436540").send(title);
+    i.forEach((data) => {
+      const embed = new MessageEmbed()
+        .setColor("#f2f2f2")
+        .setTitle(data.title)
+        .setURL(data.productURL)
+        .setDescription(data.description)
+        .addFields(
+          {
+            name: "Started on",
+            value: data.startDate,
+            inline: true,
+          },
+          {
+            name: "Ends on",
+            value: `${data.endDate} ${
+              data.endDateDiffernce > 1
+                ? `( In ${data.endDateDiffernce} days )`
+                : `( In ${data.endDateDiffernce} day )`
+            }`,
+            inline: true,
+          }
+        )
+        .setImage(data.imageURL)
+        .setTimestamp()
+        .setFooter("Made by eliac7#5541");
+      client.channels.cache.get("709045710204436540").send({ embeds: [embed] });
+    });
   });
 });
 
